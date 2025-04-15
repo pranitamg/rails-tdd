d15537d3148529bdb1bec926b89dd64952de79e9
pranitamg <gawade.pranita21@gmail.com>
2025-04-15 14:20:00 +0530

    Add code to pass 8th failed test

--- app/services/add.rb
+++ app/services/add.rb
@@ -4,6 +4,8 @@ class Add
 
         raise ArgumentError, "Empty array provided" if values.empty?
 
+        raise ArgumentError, "All elements must be valid numbers" if !values.all? { |v| v.is_a?(Numeric) }
+
         values.sum
     end
 end
