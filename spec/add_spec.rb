99fd9e461ccdf79d107f18fb4d034f67c9ed31b1
pranitamg <gawade.pranita21@gmail.com>
2025-04-15 12:47:35 +0530

    Update: Third failed test

--- spec/add_spec.rb
+++ spec/add_spec.rb
@@ -13,9 +13,15 @@ RSpec.describe Add do
         expect(add).to respond_to(:addition)
     end
 
-    # checks if it adds two numbers
-    it "adds two numbers" do
+    # checks whether the addition method takes arguments to perform addition
+    it "addition method takes arguments for addition" do
         add = Add.new.addition(2, 3)
-        expect(add).to eq(5)
+        expect(add). to eq(5)
     end
+
+    # checks if it adds two numbers
+    # it "adds two numbers" do
+    #     add = Add.new.addition(2, 3)
+    #     expect(add).to eq(5)
+    # end
 end
