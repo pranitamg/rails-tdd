158194780f01919b1cc24de8c9f7f89360ff9d43
pranitamg <gawade.pranita21@gmail.com>
2025-04-15 11:35:14 +0530

    Add code to pass first test

--- spec/add_spec.rb
+++ spec/add_spec.rb
@@ -1,4 +1,7 @@
+require './app/services/add.rb'
+
 RSpec.describe Add do
+    # checks whether the Add class exists
     it "creates an Add class" do
         add = Add.new
         expect(add).to be_kind_of(Add)
